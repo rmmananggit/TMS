5a8adf0b16d77d3a1ab651b72e25a6cb2289f811
Rochell Mark Mananggit <107897402+rmmananggit@users.noreply.github.com>
2023-11-18 16:33:59 +0800

    registration

--- asd.php
+++ asd.php
@@ -1,36 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    
-    <!-- Style -->
-    <link rel="stylesheet" href="css/style.css">
-
-    <title>TMS: Registration</title>
-  </head>
-  <body>
-  
-
-  <div class="d-lg-flex half">
-    <div class="bg order-1 order-md-2" style="background-image: url('images/register.png');"></div>
-    <div class="contents order-2 order-md-1">
-
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-7 py-3">
-            <h3><b>Registration Page 1</b></h3>
-            <p class="mb-4" style="color: black;">Empowering Minds, Crafting Futures - A Nexus of Innovation and Education!</p>
-            <form action="processpage1.php" method="post">
+<form action="#" method="post">
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group first">
@@ -75,14 +43,13 @@
               </div>
               <div class="row">
                 <div class="col-md-6">
-              
                   <div class="form-group last mb-3">
                     <label for="password">Password</label>
                     <input type="password" class="form-control" placeholder="Your Password" id="password">
                   </div>
                 </div>
+
                 <div class="col-md-6">
-              
                   <div class="form-group last mb-3">
                     <label for="re-password">Confirm Password</label>
                     <input type="password" class="form-control" placeholder="Your Password" id="re-password">
@@ -102,6 +69,9 @@
                     <input type="date" class="form-control" name="birthday">
                   </div>    
                 </div>
+              </div>
+
+              <div class="row">
                 <div class="col-md-6">
                   <div class="form-group first">
                     <label for="purok">Purok</label>
@@ -114,23 +84,53 @@
                     <input type="text" class="form-control" placeholder="e.g. San Juan" name="barangay">
                   </div>    
                 </div>
+  
+              
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group first">
+                    <label for="picture">Profile Picture</label>
+                    <input class="form-control" type="file" name="picture" accept="image/jpeg, image/png">
+                  </div>    
+                </div>
+              </div>
+              <div class="row">
+
+              <div class="col-md-12">
+                  <div class="form-group first">
+                    <label for="suffix">Role</label>
+                    <select class="form-control" name="role">
+                    <option selected>Select Role</option>
+                      <option value="2">Tutor</option>
+                      <option value="3">Tutee</option>
+                    </select>
+                  </div>    
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group last mb-3">
+                    <label for="re-password">Confirm Password</label>
+                    <input type="password" class="form-control" placeholder="Your Password" id="re-password">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group first">
+                    <label for="lname">Phone Number</label>
+                    <input type="text" class="form-control" placeholder="09123456789" name="phonenumber">
+                  </div>    
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group first">
+                    <label for="lname">Birthday</label>
+                    <input type="date" class="form-control" name="birthday">
+                  </div>    
+                </div>
+              </div>
+
 
               <input type="submit" value="Next" class="btn px-5 btn-primary mt-4">
 
             </form>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    
-  </div>
-    
-    
-
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
-  </body>
-</html>
--- registration/page2.php
+++ registration/page2.php
@@ -1,121 +1,114 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    
-    <!-- Style -->
-    <link rel="stylesheet" href="css/style.css">
-
-    <title>TMS: Registration</title>
-  </head>
-  <body>
-  
-
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Style -->
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <title>TMS: Registration</title>
+  <style>
+    .hidden {
+      display: none;
+    }
+    .rounded-circle {
+      border-radius: 50%;
+    }
+  </style>
+</head>
+<body>
   <div class="d-lg-flex half">
     <div class="bg order-1 order-md-2" style="background-image: url('images/register.png');"></div>
     <div class="contents order-2 order-md-1">
-
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-md-7 py-5">
             <h3><b>Registration Page 2</b></h3>
             <p class="mb-4" style="color: black;">Empowering Minds, Crafting Futures - A Nexus of Innovation and Education!</p>
-            <form action="#" method="post">
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="form-group first">
-                    <label for="purok">Purok</label>
-                    <input type="text" class="form-control" placeholder="e.g. 1" name="purok">
-                  </div>    
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group first">
-                    <label for="barangay">Barangay</label>
-                    <input type="text" class="form-control" placeholder="e.g. San Juan" name="barangay">
-                  </div>    
-                </div>
-  
-              
-              </div>
+            <form action="#" method="post" enctype="multipart/form-data">
+
               <div class="row">
                 <div class="col-md-12">
                   <div class="form-group first">
-                    <label for="picture">Profile Picture</label>
-                    <input class="form-control" type="file" name="picture" accept="image/jpeg, image/png">
-                  </div>    
-                </div>
-              </div>
-              <div class="row">
-
-              <div class="col-md-6">
-                  <div class="form-group first">
-                    <label for="suffix">Role</label>
-                    <select class="form-control" name="role">
-                    <option selected>Select Role</option>
-                      <option value="2">Tutor</option>
-                      <option value="3">Tutee</option>
-                    </select>
-                  </div>    
-                </div>
-
-                <div class="col-md-6">
-                  <div class="form-group last mb-3">
-                    <label for="re-password">Confirm Password</label>
-                    <input type="password" class="form-control" placeholder="Your Password" id="re-password">
+                  <label for="Profile Picture">Profile Picture:</label>
+                  <img id="previewImage" class="rounded-circle img-fluid hidden mt-3 mb-3" width="200" height="200" style="display: block; margin-left: auto; margin-right: auto;">
+                    <input class="form-control" type="file" name="picture" accept="image/jpeg, image/png" id="profilePicture">
+                   
                   </div>
                 </div>
               </div>
               <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-12">
                   <div class="form-group first">
-                    <label for="lname">Phone Number</label>
-                    <input type="text" class="form-control" placeholder="09123456789" name="phonenumber">
-                  </div>    
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group first">
-                    <label for="lname">Birthday</label>
-                    <input type="date" class="form-control" name="birthday">
-                  </div>    
+                    <label for="suffix">Register As:</label>
+                    <select class="form-control" name="role" id="roleSelect" onchange="toggleButton()">
+                      <option selected disabled>Select Role</option>
+                      <option value="2">Tutor</option>
+                      <option value="3">Tutee</option>
+                    </select>
+                  </div>
                 </div>
               </div>
 
-              
-              <div class="d-flex mb-5 mt-4 align-items-center">
-                <div class="d-flex align-items-center">
-                <label class="control control--checkbox mb-0"><span class="caption">Creating an account means you're okay with our <a href="#">Terms and Conditions</a> and our <a href="#">Privacy Policy</a>.</span>
-                  <input type="checkbox" required/>
-                  <div class="control__indicator"></div>
-                </label>
-              </div>
-              </div>
-
-              <input type="submit" value="Submit" class="btn px-5 btn-primary">
+              <input type="submit" value="Submit" class="btn px-5 btn-primary" id="submitButton">
 
             </form>
           </div>
         </div>
       </div>
     </div>
-
-    
   </div>
-    
-    
 
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
-  </body>
+  <script>
+    function toggleButton() {
+      var roleSelect = document.getElementById('roleSelect');
+      var submitButton = document.getElementById('submitButton');
+
+      if (roleSelect.value === '3') { // '3' corresponds to the value for Tutee
+        submitButton.value = 'Submit';
+      } else if (roleSelect.value === '2') { // '2' corresponds to the value for Tutor
+        submitButton.value = 'Next';
+      }
+    }
+
+    function toggleAdditionalInfo() {
+      var roleSelect = document.getElementById('roleSelect');
+      var previewImage = document.getElementById('previewImage');
+
+      if (roleSelect.value === '2') { // '2' corresponds to the value for Tutor
+        previewImage.classList.remove('hidden');
+      } else {
+        previewImage.classList.add('hidden');
+      }
+    }
+
+    // Display the uploaded image
+    document.getElementById('profilePicture').addEventListener('change', function() {
+      var previewImage = document.getElementById('previewImage');
+      var fileInput = this;
+
+      if (fileInput.files && fileInput.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+          previewImage.src = e.target.result;
+          previewImage.classList.remove('hidden');
+        };
+
+        reader.readAsDataURL(fileInput.files[0]);
+      }
+    });
+  </script>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
 </html>
